8c0ad1cf6bf1da745c3018826763a7a09a66b9ee
Priyanshu Kumar <rohitksrj@gmail.com>
2026-06-15 13:26:23 +0530

    Switching to stable direct database string

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 # --- PURANI DATABASE VALI LINE KO HATAKAR YEH DO LINES LIKHO ---
 
 # 1. Jo aapne Render se copy kiya (External URL) use yahan paste karo:
-EXTERNAL_DATABASE = 'postgresql://postgres.ovgfbumulchtzyjimgdt:%40Pksm887314@aws-0-ap-south-1.pooler.supabase.com:6543/postgres?sslmode=require&options=-c%20search_path%3Dpublic'
+EXTERNAL_DATABASE = 'postgresql://postgres:%40Pksm887314@db.ovgfbumulchtzyjimgdt.supabase.co:5432/postgres'
 
 # 2. Yeh line dono ko handle karegi (Laptop par external use karegi, Render par internal)
 DATABASE = os.environ.get('DATABASE_URL', EXTERNAL_DATABASE)
